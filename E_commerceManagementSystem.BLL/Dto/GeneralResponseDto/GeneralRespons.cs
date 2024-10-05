1d70f6ae56dfc184cc231b8a5a56f5fe990576d4
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-05 05:14:28 +0300

    E_commerceManagementSystem.BLL.DTOs.GeneralResponseDto | rename GeneralResponsDto to GeneralRespons

--- E_commerceManagementSystem.BLL/Dto/GeneralResponseDto/GeneralRespons.cs
+++ E_commerceManagementSystem.BLL/Dto/GeneralResponseDto/GeneralRespons.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace E_commerceManagementSystem.BLL.DTOs.GeneralResponseDto
 {
-    public class GeneralResponsDto
+    public class GeneralRespons
     {
-        public bool Successe { get; set; } = false;
-
+        public bool Success { get; set; } = false;
+        public string Message { get; set; }
         public List<string>? Errors { get; set; }
+        public object? Model { get; set; }
+       
 
     }
 }
