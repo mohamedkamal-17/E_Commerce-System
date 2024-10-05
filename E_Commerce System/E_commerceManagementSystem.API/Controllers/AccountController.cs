45d89a25e35b3ab77a258b02a340700071aa4ed1
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-06 00:14:31 +0300

    fix bug in Vs

---
+++ E_Commerce System/E_commerceManagementSystem.API/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using E_commerceManagementSystem.BLL.DTOs;
+using E_commerceManagementSystem.BLL.DTOs.AccountDto;
+using E_commerceManagementSystem.BLL.DTOs.GeneralResponseDto;
+using E_commerceManagementSystem.BLL.Manager.AccountManager;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountManager _AccountManager;
+        private readonly RoleManager<IdentityRole> _RoleManager;
+
+        public AccountsController(IAccountManager accountManager,RoleManager<IdentityRole> roleManager)
+        {
+            _AccountManager = accountManager;
+            _RoleManager = roleManager;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegistarAsync([FromBody]UserRegisterDTO UserRegisterDTO )
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            GeneralRespons result=await _AccountManager.RegisterAsync(UserRegisterDTO);
+            if (!result.Success)
+                return BadRequest(result);
+
+            else
+                return Ok("User registered successfully!");
+        }
+
+        [HttpPost("login")]
+       
+        public async Task<IActionResult>LoginAsync([FromBody] UserLoginDTO UserLoginDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            TokenRespons result = await _AccountManager.LoginAsync(UserLoginDTO);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+                return BadRequest("User name or Password is not valid");
+        }
+    }
+}
---
+++ E_Commerce System/E_commerceManagementSystem.API/Program.cs
@@ -0,0 +1,59 @@
+using E_commerceManagementSystem.DAL.Data.Dphelper;
+using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.BLL.DTOs;
+
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using E_commerceManagementSystem.BLL.Manager.AccountManager;
+using E_commerceManagementSystem.BLL.Manager.JwtTokenManager;
+
+
+namespace E_Commerce_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+
+
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("E_Commerce_string"));
+            });
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                            .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllers();
+
+            builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+            builder.Services.AddScoped<IAccountManager, AccountMangare>();
+            // register for the service 
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
