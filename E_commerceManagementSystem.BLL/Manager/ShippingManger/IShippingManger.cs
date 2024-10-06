9784fd1636560546c509178e075a1c16e7df91f6
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-06 07:08:34 +0300

    E_commerceManagementSystem.BLL.Manager.ShippingManger | create shipping manger

---
+++ E_commerceManagementSystem.BLL/Manager/ShippingManger/IShippingManger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_commerceManagementSystem.BLL.Dto.ShippingDto;
+using E_commerceManagementSystem.BLL.Manager.GeneralManager;
+using E_commerceManagementSystem.DAL.Data.Models;
+namespace E_commerceManagementSystem.BLL.Manager.ShippingManger
+{
+    public interface IShippingManger:IManager<Shipping,AddShippingDto,UpdateShippingDto>
+    {
+    }
+}
