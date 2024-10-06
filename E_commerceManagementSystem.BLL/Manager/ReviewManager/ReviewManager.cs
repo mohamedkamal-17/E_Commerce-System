af13105443240bd49286a53ca4b332617a008718
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-06 12:59:54 +0300

    complete review manager

--- E_commerceManagementSystem.BLL/Manager/ReviewManager/ReviewManager.cs
+++ E_commerceManagementSystem.BLL/Manager/ReviewManager/ReviewManager.cs
@@ -2,9 +2,13 @@
 using E_commerceManagementSystem.BLL.Dto.ReviewDto;
 using E_commerceManagementSystem.BLL.DTOs.GeneralResponseDto;
 using E_commerceManagementSystem.BLL.Manager.GeneralManager;
+using E_commerceManagementSystem.BLL.Manager.ProductManager;
 using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.DAL.Reposatories.ProductRepository;
 using E_commerceManagementSystem.DAL.Reposatories.ReviewRepository;
 using E_commerceManagementSystem.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,20 +20,90 @@ namespace E_commerceManagementSystem.BLL.Manager.ReviewManager
     public class ReviewManager : Manager<Review, AddReviewDto, UpdateReviewDto>, IReviewManager
     {
         private readonly IReviewRepo _reviewRepo;
-        public ReviewManager(IReviewRepo repository, IMapper mapper) 
+        private readonly IProductRepo _productRepo;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ReviewManager(IReviewRepo repository, IMapper mapper, IProductRepo productRepo, UserManager<ApplicationUser> userManager)
             : base(repository, mapper)
         {
             _reviewRepo = repository;
+            _productRepo = productRepo;
+            _userManager = userManager;
         }
 
-        public Task<GeneralRespons> GetByProductIdAsync(int productid)
+        public async Task<GeneralRespons> GetByProductIdAsync(int productid)
         {
-            throw new NotImplementedException();
+            var productExists = await _productRepo.GetByIdAsync(productid);
+            if(productExists == null)
+            {
+                return new GeneralRespons
+                {
+                    Success = false,
+                    Message = "No Product with this id"
+                };
+            }
+            var review = await _reviewRepo.GetByProductIdAsync(productid);
+            if(await review.CountAsync() == 0)
+            {
+                return new GeneralRespons
+                {
+                    Success = false,
+                    Message = "No reviews for this product"
+                };
+            }
+            var reviews = await review.ToListAsync();
+
+            var reviewDtos = reviews.Select(r => new ReadReviewDto
+            {
+                ProductName = r.Product.ProductName,
+                UserName = r.User.UserName,
+                Rating = r.Rating,
+                ReviewText = r.ReviewText,
+                CreatedDate = r.CreatedDate
+            }).ToList();
+
+            return new GeneralRespons
+            {
+                Success = true,
+                Model = reviewDtos
+            };
         }
 
-        public Task<GeneralRespons> GetByUserIdAsync(string userId)
+        public async Task<GeneralRespons> GetByUserIdAsync(string userId)
         {
-            throw new NotImplementedException();
+            var userExists = await _userManager.FindByIdAsync(userId);
+            if (userExists == null)
+            {
+                return new GeneralRespons
+                {
+                    Success = false,
+                    Message = "No user with this id"
+                };
+            }
+            var review = await _reviewRepo.GetByUserIdAsync(userId);
+            if (await review.CountAsync() == 0)
+            {
+                return new GeneralRespons
+                {
+                    Success = false,
+                    Message = "this user have not any reviews"
+                };
+            }
+            var reviews = await review.ToListAsync();
+
+            var reviewDtos = reviews.Select(r => new ReadReviewDto
+            {
+                ProductName = r.Product.ProductName,
+                UserName = r.User.UserName,
+                Rating = r.Rating,
+                ReviewText = r.ReviewText,
+                CreatedDate = r.CreatedDate
+            }).ToList();
+
+            return new GeneralRespons
+            {
+                Success = true,
+                Model = reviewDtos
+            };
         }
     }
 }
