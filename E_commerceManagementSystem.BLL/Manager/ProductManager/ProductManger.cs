0eb0dd2148337ecf2d1388705fa34b6e6977a774
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-06 03:36:52 +0300

    E_commerceManagementSystem.BLL.Manager.ProductManager | build Product Manger

--- E_commerceManagementSystem.BLL/Manager/ProductManager/ProductManger.cs
+++ E_commerceManagementSystem.BLL/Manager/ProductManager/ProductManger.cs
@@ -1,24 +1,29 @@
-﻿//using E_commerceManagementSystem.BLL.Manager.GeneralManager;
-//using E_commerceManagementSystem.DAL.Data.Models;
-//using E_commerceManagementSystem.DAL.Repositories.Interfaces;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using AutoMapper;
+using E_commerceManagementSystem.BLL.Dto.ProductDto;
+using E_commerceManagementSystem.BLL.Manager.GeneralManager;
+using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace E_commerceManagementSystem.BLL.Manager.ProductManager
-//{
-//    public class ProductManger : Manager<Product, TAddDto, TUpdateDto>, IProductMangare
-//    {
-//        ProductManger(IRepository<Product> repo) : base(repo) { }
-//        public Task<ICollection<Product>> GetProductByCategoryNameAsync(string Name)
-//        {
-//            throw new NotImplementedException();
-//        }
+namespace E_commerceManagementSystem.BLL.Manager.ProductManager
+{
+    public class ProductManger : Manager<Product, AddproductDto, UpdateProductDto>, IProductMangare
+    {
+        public ProductManger(IRepository<Product> repository, IMapper mapper)
+             : base(repository, mapper)
+        {
+        }
+        public Task<ICollection<Product>> GetProductByCategoryNameAsync(string Name)
+        {
+            throw new NotImplementedException();
+        }
 
 
 
 
-//    }
-//}
+    }
+}
