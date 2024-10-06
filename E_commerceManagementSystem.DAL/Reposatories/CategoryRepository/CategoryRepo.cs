40d668f7de5ab64e371a408e6a69c6ecb404cca8
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-06 06:31:43 +0300

    E_commerceManagementSystem.DAL.Reposatories.CategoryRepository | add category repo

---
+++ E_commerceManagementSystem.DAL/Reposatories/CategoryRepository/CategoryRepo.cs
@@ -0,0 +1,16 @@
+﻿using E_commerceManagementSystem.DAL.Data.Dphelper;
+using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.DAL.Reposatories.GeneralRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.DAL.Reposatories.CategoryRepository
+{
+    public class CategoryRepo:Repository<Product>,ICategoryRepo
+    {
+        CategoryRepo(ApplicationDbContext context):base(context) { }
+    }
+}
