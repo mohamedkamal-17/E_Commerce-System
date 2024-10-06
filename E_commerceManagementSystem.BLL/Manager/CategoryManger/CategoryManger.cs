9cfe3e9f72f6db1baf450e05880857ae2cc74506
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-06 06:31:19 +0300

    E_commerceManagementSystem.BLL.Manager.CategoryManger | AddCategory Manger

---
+++ E_commerceManagementSystem.BLL/Manager/CategoryManger/CategoryManger.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using E_commerceManagementSystem.BLL.Dto.CategoryDto;
+using E_commerceManagementSystem.BLL.Manager.GeneralManager;
+using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.DAL.Reposatories.CategoryRepository;
+using E_commerceManagementSystem.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.BLL.Manager.CategoryManger
+{
+    public class CategoryManger:Manager<Category, AddCategoryDTO, UpdateCategoryDto>, ICategoryManger
+      
+    {
+        public CategoryManger(IRepository<Category> repository,IMapper mapper):base(repository, mapper) { }
+    }
+}
---
+++ E_commerceManagementSystem.BLL/Manager/CategoryManger/ICategoryManger.cs
@@ -0,0 +1,16 @@
+﻿using E_commerceManagementSystem.BLL.Dto.CategoryDto;
+using E_commerceManagementSystem.BLL.Dto.ProductDto;
+using E_commerceManagementSystem.BLL.Manager.GeneralManager;
+using E_commerceManagementSystem.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.BLL.Manager.CategoryManger
+{
+    public interface ICategoryManger: IManager<Category, AddCategoryDTO, UpdateCategoryDto>
+    {
+    }
+}
