aab00f42393e68fb90b239dcb9bc1aa675f2d75c
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-06 07:09:43 +0300

    E_commerceManagementSystem.DAL.Reposatories.ShippingRepository | Create Shipping repo

---
+++ E_commerceManagementSystem.DAL/Reposatories/ShippingRepository/ShippingRepo.cs
@@ -0,0 +1,25 @@
+﻿using E_commerceManagementSystem.DAL.Data.Dphelper;
+using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.DAL.Reposatories.GeneralRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.DAL.Reposatories.ShippingRepository
+{
+    public class ShippingRepo:Repository<Shipping>
+    {
+        private readonly ApplicationDbContext context;
+
+        public ShippingRepo(ApplicationDbContext Context):base(Context)
+        {
+            context = Context;
+        }
+        
+            
+        }
+
+    }
+
