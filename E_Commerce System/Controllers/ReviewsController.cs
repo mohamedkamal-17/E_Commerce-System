81a5cce3d252c721f9d29c4570a6f40ffe0169c1
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-06 13:00:14 +0300

    add get all to review controller

---
+++ E_Commerce System/Controllers/ReviewsController.cs
@@ -0,0 +1,29 @@
+﻿using E_commerceManagementSystem.BLL.Manager.ReviewManager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IReviewManager _reviewManager;
+
+        public ReviewsController(IReviewManager reviewManager)
+        {
+            _reviewManager = reviewManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var result = await _reviewManager.GetAllAsync();
+            if (!result.Success)
+            {
+                return NotFound(result.Message);
+            }
+            return Ok(result.Model);
+        }
+    }
+}
