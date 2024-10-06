5efbfac542895130b542942b16e7256c2a5dc1c2
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-06 05:05:08 +0300

    add reviewDto

---
+++ E_commerceManagementSystem.BLL/Dto/ReviewDto/AddReviewDto.cs
@@ -0,0 +1,17 @@
+﻿using E_commerceManagementSystem.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.BLL.Dto.ReviewDto
+{
+    public class AddReviewDto
+    {
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+        public int ProductId { get; set; } 
+        public string UserId { get; set; } 
+    }
+}
---
+++ E_commerceManagementSystem.BLL/Dto/ReviewDto/ReadReviewDto.cs
@@ -0,0 +1,19 @@
+﻿using E_commerceManagementSystem.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.BLL.Dto.ReviewDto
+{
+    public class ReadReviewDto
+    {
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string ProductName { get; set; }
+        public string UserName { get; set; } 
+
+    }
+}
