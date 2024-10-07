e25c4707aa8963f5c20d9959c6e63501dd5746ef
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-07 05:07:43 +0300

    E_commerceManagementSystem.BLL.Manager.CategoryManger | Complite Category manager

--- E_commerceManagementSystem.BLL/Manager/CategoryManger/ICategoryManger.cs
+++ E_commerceManagementSystem.BLL/Manager/CategoryManger/ICategoryManger.cs
@@ -1,5 +1,6 @@
 ﻿using E_commerceManagementSystem.BLL.Dto.CategoryDto;
 using E_commerceManagementSystem.BLL.Dto.ProductDto;
+using E_commerceManagementSystem.BLL.DTOs.GeneralResponseDto;
 using E_commerceManagementSystem.BLL.Manager.GeneralManager;
 using E_commerceManagementSystem.DAL.Data.Models;
 using System;
@@ -10,7 +11,8 @@ using System.Threading.Tasks;
 
 namespace E_commerceManagementSystem.BLL.Manager.CategoryManger
 {
-    public interface ICategoryManger: IManager<Category, AddCategoryDTO, UpdateCategoryDto>
+    public interface ICategoryManger: IManager<Category, ReadCategoryDto, AddCategoryDTO, UpdateCategoryDto>
     {
+        Task<GeneralRespons> GetByCategoryNameAsync(string categoryName);
     }
 }
