6b91e0b3a4106fa3c1e45e00bafa32f5f2242982
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-07 04:52:03 +0300

    E_Commerce_System.Controllers |
    Add All end Point To Product Controller

--- E_Commerce System/Controllers/ProductsController.cs
+++ E_Commerce System/Controllers/ProductsController.cs
@@ -44,6 +44,10 @@ namespace E_Commerce_System.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<GeneralRespons>> GetById(int id)
         {
+            if(id <= 0)
+            {
+                return BadRequest(new { meassge = "id shoud larg Than Zero" });
+            }
             var response = await _productMangare.GetByIdAsync(id);
             if (!response.Success)
             {
@@ -52,11 +56,100 @@ namespace E_Commerce_System.Controllers
             return Ok(response);
         }
 
-        //Task<GeneralRespons> GetAllAsync();
-        //Task<GeneralRespons> GetByIdAsync(int id);
+        //Task<GeneralRespons> GetByCategoryNameAsync(string categoryName);
+        [HttpGet("{PrpductName")]
+        public async Task<ActionResult<GeneralRespons>> GetByProductName(string PrpductName)
+        {
+            if (PrpductName == null)
+            {
+                return BadRequest(new { message = "shoud Enter PrpductName" });
+            }
+            var response = await _productMangare.GetByProductNameAsync(PrpductName);
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+        [HttpGet("{CategoryName}")]
+        public async Task<ActionResult<GeneralRespons>> GetByCategoryName(string CategoryName)
+        {
+            if (CategoryName == null)
+            {
+                return BadRequest(new { message = "shoud Enter CategoryName" });
+            }
+            var response = await _productMangare.GetByCategoryNameAsync(CategoryName);
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
 
+        }
+        //Task<GeneralRespons> GetByPraiceAsync(float price);
+        [HttpGet("{price}")]
+        public async Task<ActionResult<GeneralRespons>> GetByPraiceAsync(float price)
+        {
+            if (price < 0)
+            {
+                return BadRequest(new { message = "shoud Enter price" });
+            }
+            var response = await _productMangare.GetByPraiceAsync(price);
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
 
-        //Task<GeneralRespons> AddAsync(TAddDto dto);
+        }
+
+        //Task<GeneralRespons> GetByPraiceInRangeAsync(float highPrice, float lowPrice);
+        [HttpGet("{highPrice}/{lowPrice}")]
+        public async Task<ActionResult<GeneralRespons>> GetByPraiceInRangeAsync(float highPrice, float lowPrice)
+        {
+
+            if (highPrice < 0 || lowPrice < 0)
+            {
+                return BadRequest(new{ message= "highPrice or lowPrice not Exit "});
+            }
+            var response = await _productMangare.GetByPraiceInRangeAsync(highPrice,lowPrice);
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+
+        }
+        [HttpGet("{lowPrice}")]
+        public async Task<ActionResult<GeneralRespons>> GetByPraicelargthanAsync(float lowPrice)
+        {
+            if (lowPrice < 0)
+            {
+                return BadRequest(new { message = "highPrice or lowPrice not Exit " });
+            }
+            var response = await _productMangare.GetByPraicelargthanAsync( lowPrice);
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+        //Task<GeneralRespons> GetByPraicelessthanAsync(float highPrice);
+        [HttpGet("{lowPrice}")]
+        public async Task<ActionResult<GeneralRespons>> GetByPraiceLessthanAsync(float largPrice)
+        {
+            if (largPrice < 0)
+            {
+                return BadRequest(new { message = "highPrice or lowPrice not Exit " });
+            }
+            var response = await _productMangare.GetByPraicelessthanAsync(largPrice);
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+
 
         [HttpPost]
         public async Task<ActionResult<GeneralRespons>> Add(AddproductDto model)
@@ -71,12 +164,12 @@ namespace E_Commerce_System.Controllers
             {
                 return NotFound(response);
             }
-
+           
             return CreatedAtAction(nameof(GetById), new { id = (response.Model as Product)?.Id }, response); // Return 201 Created
 
 
         }
-        //Task<GeneralRespons> UpdateAsync(TUpdateDto dto);
+        
         [HttpPut("{id}")]
         public async Task<ActionResult<GeneralRespons>> Update(int id,[FromBody] UpdateProductDto model)
         {
