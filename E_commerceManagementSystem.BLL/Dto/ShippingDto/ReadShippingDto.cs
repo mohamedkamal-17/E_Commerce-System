cfb70715afc5fdd2168b8ea9bf54ca3e40712d58
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-07 05:06:55 +0300

    E_commerceManagementSystem.BLL.Dto.ShippingDto | Add propprty To Shipping Dto

--- E_commerceManagementSystem.BLL/Dto/ShippingDto/ReadShippingDto.cs
+++ E_commerceManagementSystem.BLL/Dto/ShippingDto/ReadShippingDto.cs
@@ -1,5 +1,4 @@
-﻿using E_commerceManagementSystem.DAL.Data.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +6,11 @@ using System.Threading.Tasks;
 
 namespace E_commerceManagementSystem.BLL.Dto.ShippingDto
 {
-    public class AddShippingDto
+    public class ReadShippingDto
     {
-       
-
+        public string UserId { get; set; }
         public int OrderId { get; set; } // Foreign Key referencing Orders
-       
+
         public string ShippingMethod { get; set; } // e.g., Standard, Express, etc.
         public int TrackingNumber { get; set; }
         public decimal ShippingCost { get; set; }
