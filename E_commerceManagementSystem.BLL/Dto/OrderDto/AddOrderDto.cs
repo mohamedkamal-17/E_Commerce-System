2d72e47e354e7ecea4cf007e444c5f98ec7feb5c
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-07 05:25:05 +0300

    add dto to order and order item

---
+++ E_commerceManagementSystem.BLL/Dto/OrderDto/AddOrderDto.cs
@@ -0,0 +1,18 @@
+﻿using E_commerceManagementSystem.BLL.Dto.OrederItemDto;
+using E_commerceManagementSystem.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.BLL.Dto.OrderDto
+{
+    public class AddOrderDto
+    {
+        public string UserId { get; set; }
+        public string Address { get; set; }
+        public int PaymentId { get; set; }  
+        public ICollection<AddOrderItemDto>? addOrderItemDtos { get; set; }
+    }
+}
