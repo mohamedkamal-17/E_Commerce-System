1cb61fc227e7e101afe833ff13ea3c029225014d
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-07 19:39:10 +0300

    add cart and cart item repo

---
+++ E_commerceManagementSystem.DAL/Reposatories/CartItemRepository/CartItemRepo.cs
@@ -0,0 +1,27 @@
+﻿using E_commerceManagementSystem.DAL.Data.Dphelper;
+using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.DAL.Reposatories.GeneralRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.DAL.Reposatories.CartItemRepository
+{
+    public class CartItemRepo : Repository<CartItem> , ICartItemRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartItemRepo(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IQueryable<CartItem>> GetByCartIdAsync(int cartId)
+        {
+            return _context.CartItems.Where(x => x.CartID == cartId).AsNoTracking();
+        }
+    }
+}
