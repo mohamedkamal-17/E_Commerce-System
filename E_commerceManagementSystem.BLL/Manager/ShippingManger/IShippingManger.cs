4e099ba711fc3f270e15fea138f4e865a27c67a0
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-07 05:13:52 +0300

    E_commerceManagementSystem.BLL.Manager.ShippingManger | Create Shipping Manager

--- E_commerceManagementSystem.BLL/Manager/ShippingManger/IShippingManger.cs
+++ E_commerceManagementSystem.BLL/Manager/ShippingManger/IShippingManger.cs
@@ -9,11 +9,11 @@ using E_commerceManagementSystem.BLL.Manager.GeneralManager;
 using E_commerceManagementSystem.DAL.Data.Models;
 namespace E_commerceManagementSystem.BLL.Manager.ShippingManger
 {
-    public interface IShippingManger:IManager<Shipping,AddShippingDto,UpdateShippingDto>
+    public interface IShippingManger:IManager<Shipping, ReadShippingDto, AddShippingDto, UpdateShippingDto>
     {
 
         Task<GeneralRespons> GetByOrderIdAsync(int id);
-        Task<GeneralRespons> GetBytrackingnumber(int id);
+        Task<GeneralRespons> GetByTrackingNumber(int trackingNumber);
         Task<GeneralRespons> GetByShippingDate(DateTime shippingDate);
         Task<GeneralRespons> GetByShipingState(string statu);
 
--- E_commerceManagementSystem.BLL/Manager/ShippingManger/ShippingManger.cs
+++ E_commerceManagementSystem.BLL/Manager/ShippingManger/ShippingManger.cs
@@ -10,37 +10,127 @@ using E_commerceManagementSystem.BLL.Manager.GeneralManager;
 using E_commerceManagementSystem.DAL.Data.Models;
 using E_commerceManagementSystem.DAL.Reposatories.ProductRepository;
 using E_commerceManagementSystem.DAL.Reposatories.ShippingRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_commerceManagementSystem.BLL.Manager.ShippingManger
 {
-    public class ShippingManger:Manager<Shipping,AddShippingDto,UpdateShippingDto>,IShippingManger
+    public class ShippingManger:Manager<Shipping, ReadShippingDto,AddShippingDto, UpdateShippingDto>,IShippingManger
     {
+        private readonly IShippingRepo _rpository;
+        private readonly IMapper _mapper;
         
-   
-    public ShippingManger(IShippingRepo rpository, IMapper mapper)
+        public ShippingManger(IShippingRepo rpository, IMapper mapper)
        
    
-        : base(rpository, mapper) { }
-
-
-        public Task<GeneralRespons> GetByOrderIdAsync(int id)
+        : base(rpository, mapper)
         {
-            throw new NotImplementedException();
+            _rpository = rpository;
+           _mapper = mapper;
+           
         }
 
-        public Task<GeneralRespons> GetByShipingState(string statu)
+        private GeneralRespons CreateResponse(bool success, object? model, string message, List<string>? errors = null)
         {
-            throw new NotImplementedException();
+            return new GeneralRespons
+            {
+                Success = success,
+                Model = model,
+                Message = message,
+                Errors = errors ?? new List<string>()
+            };
+        }
+        public async Task<GeneralRespons> GetByOrderIdAsync(int id)
+        {
+
+            try
+            {
+                var qurabelList = await _rpository.GetAllAsync();
+                var order = qurabelList.FirstOrDefault(or => or.Id == id);
+
+                if (order != null)
+                {
+                    ReadShippingDto shipingdto = _mapper.Map<ReadShippingDto>(order);
+                    return CreateResponse(true,
+                        shipingdto, $"{typeof(Shipping).Name} retrieved successfully", null);
+                }
+                return CreateResponse(false, null, $"{typeof(Shipping).Name} not  retrieved successfully", new List<string> { "Not found Order With This Id" });
+            }
+            catch (Exception ex) { 
+            
+            return CreateResponse(false,null, ex.Message, new List<string>{ "Server sid Error"});
+            
+            }
         }
 
-        public Task<GeneralRespons> GetByShippingDate(DateTime shippingDate)
+        public async Task<GeneralRespons> GetByShipingState(string shipingStatu)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var qurabelList = await _rpository.GetAllAsync();
+                var orders = await qurabelList.Where(or => or.ShippingStatus == shipingStatu).ToListAsync();
+
+                if (orders != null)
+                {
+                    var shipingdto = _mapper.Map<List<ReadShippingDto>>(orders);
+                    return CreateResponse(true,
+                        shipingdto, $"{typeof(Shipping).Name} retrieved successfully", null);
+                }
+                return CreateResponse(false, null, $"{typeof(Shipping).Name} not  retrieved successfully", new List<string> { "Not found Order With This shipingStatu" });
+            }
+            catch (Exception ex)
+            {
+
+                return CreateResponse(false, null, ex.Message, new List<string> { "Server sid Error" });
+
+            }
         }
 
-        public Task<GeneralRespons> GetBytrackingnumber(int id)
+            public async  Task<GeneralRespons> GetByShippingDate(DateTime shippingDate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var qurabelList = await _rpository.GetAllAsync();
+                var orders = await qurabelList.Where(or => or.ShippedDate == shippingDate).ToListAsync();
+
+                if (orders != null)
+                {
+                    var shipingdto = _mapper.Map<List<ReadShippingDto>>(orders);
+                    return CreateResponse(true,
+                        shipingdto, $"{typeof(Shipping).Name} retrieved successfully", null);
+                }
+                return CreateResponse(false, null, $"{typeof(Shipping).Name} not  retrieved successfully", new List<string> { "Not found Order With This shippingDate" });
+            }
+            catch (Exception ex)
+            {
+
+                return CreateResponse(false, null, ex.Message, new List<string> { "Server sid Error" });
+
+            }
+
+        }
+
+        public async Task<GeneralRespons> GetByTrackingNumber(int trackingNumber)
+        {
+            try
+            {
+                var qurabelList = await _rpository.GetAllAsync();
+                var orders = await qurabelList.Where(or => or.TrackingNumber == trackingNumber).ToListAsync();
+
+                if (orders != null)
+                {
+                    var shipingdto = _mapper.Map<List<ReadShippingDto>>(orders);
+                    return CreateResponse(true,
+                        shipingdto, $"{typeof(Shipping).Name} retrieved successfully", null);
+                }
+                return CreateResponse(false, null, $"{typeof(Shipping).Name} not  retrieved successfully", new List<string> { "Not found Order With This trackingNumber" });
+            }
+            catch (Exception ex)
+            {
+
+                return CreateResponse(false, null, ex.Message, new List<string> { "Server sid Error" });
+
+            }
+
         }
     }
 }
