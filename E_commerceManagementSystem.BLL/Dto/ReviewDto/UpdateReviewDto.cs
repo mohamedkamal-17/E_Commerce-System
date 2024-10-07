c8d3fa75cf802eb03a86c2da7af9ca35a8d59f05
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-07 05:05:42 +0300

    E_commerceManagementSystem.BLL.Dto.ReviewDto  | Create Review Dto

---
+++ E_commerceManagementSystem.BLL/Dto/ReviewDto/UpdateReviewDto.cs
@@ -0,0 +1,18 @@
+﻿using E_commerceManagementSystem.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.BLL.Dto.ReviewDto
+{
+    public class UpdateReviewDto
+    {
+        public int Id { get; set; } 
+        public int Rating { get; set; } 
+        public string ReviewText { get; set; }
+        public int ProductId { get; set; } 
+        public string UserId { get; set; } 
+    }
+}
