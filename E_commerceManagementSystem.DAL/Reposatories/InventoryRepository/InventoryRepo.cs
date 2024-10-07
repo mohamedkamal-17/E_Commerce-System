f94a26a4b79fd3945d8cd070d89b64c8a4f38338
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-07 05:18:45 +0300

    E_commerceManagementSystem.DAL.Reposatories.InventoryRepository } Create Inventory Repo

---
+++ E_commerceManagementSystem.DAL/Reposatories/InventoryRepository/InventoryRepo.cs
@@ -0,0 +1,20 @@
+﻿using E_commerceManagementSystem.DAL.Data.Dphelper;
+using E_commerceManagementSystem.DAL.Data.Models;
+using E_commerceManagementSystem.DAL.Reposatories.GeneralRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceManagementSystem.DAL.Reposatories.InventoryRepository
+{
+    public class InventoryRepo:Repository<Inventory>,IInventoryRepo
+    {
+        public InventoryRepo(ApplicationDbContext Context):base(Context)
+        {
+            
+        }
+
+    }
+}
