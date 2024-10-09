9765801c4a32aabad1c97183fa390c1e3954322d
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-09 15:15:20 +0300

    E_commerceManagementSystem.DAL.Data.Models | Update some models properrty

--- E_commerceManagementSystem.DAL/Data/Models/Payment.cs
+++ E_commerceManagementSystem.DAL/Data/Models/Payment.cs
@@ -9,11 +9,15 @@ namespace E_commerceManagementSystem.DAL.Data.Models
     public class Payment
     {
         public int Id { get; set; }
-        public DateTime? PaymentDate { get; set; }
-        public double PaymentAmount { get; set; }
-        public string PaymentMethod { get; set; }
-        public string PaymentStatus { get; set; }
+        public string PaymentIntentId {  get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
 
+        public double TotalAmount { get; set; }
+       
+        public string PaymentMethod { get; set; }
+        public string   Status{ get; set; }
+        public string Currency { get; set; }  
         public int OrderId { get; set; }          //Foreign Key  // one to one reltion
         public Order Orders { get; set; } // Navigation prop
     }
--- E_commerceManagementSystem.DAL/Data/Models/WishList.cs
+++ E_commerceManagementSystem.DAL/Data/Models/WishList.cs
@@ -8,14 +8,15 @@ namespace E_commerceManagementSystem.DAL.Data.Models
 {
     public class WishList
     {
-        public int Id { get; set; } // Primary Key
+        public int Id { get; set; }  // Primary Key
+        public string UserId { get; set; }  // Foreign Key referencing ApplicationUser
 
-        public string UserId { get; set; } // Foreign Key referencing Users
-        public ApplicationUser User { get; set; } // Navigation property
+        public  ApplicationUser User { get; set; }  // Navigation property for the user
 
-        public int ProductID { get; set; } // Foreign Key referencing Products
-        public Product Product { get; set; } // Navigation property
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;  // Timestamp for when the wishlist was created
+        public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;  // Timestamp for when the wishlist was last updated
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Timestamp
+        public  ICollection<WishListItems> WishListItems { get; set; }  // Navigation property for the wishlist items
+
     }
 }
