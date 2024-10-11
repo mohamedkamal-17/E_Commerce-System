1b7e1072056417119a4c213d8ccad65932611da0
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-11 14:30:44 +0300

    update cart read dto

--- E_commerceManagementSystem.BLL/Dto/CartDto/ReadCartDto.cs
+++ E_commerceManagementSystem.BLL/Dto/CartDto/ReadCartDto.cs
@@ -10,11 +10,12 @@ namespace E_commerceManagementSystem.BLL.Dto.CartDto
 {
     public class ReadCartDto
     {
+        public int Id { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public string UserId { get; set; } 
         public string UserName { get; set; }
-        public ICollection<ReadCartItemDto>? readCartItemDtos { get; set; }
+        public ICollection<ReadCartItemDto>? CartItems { get; set; }
             
     }
 }
