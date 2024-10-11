b10ba1852e4f9cf3b38343e6eec9b82764e2d679
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-11 19:26:17 +0300

    update cart model

--- E_commerceManagementSystem.DAL/Data/Models/CartItem.cs
+++ E_commerceManagementSystem.DAL/Data/Models/CartItem.cs
@@ -8,6 +8,7 @@ namespace E_commerceManagementSystem.DAL.Data.Models
 {
     public class CartItem
     {
+        public int Id { get; set; } 
         public int Quantity { get; set; }
         public int ProductId { get; set; }
         public virtual Product Product { get; set; }
