b9b75731fb3683635e119a3d50b090ffe6f7f194
moustafanafeaa <moustafa.anafea@gmail.com>
2024-10-11 14:30:34 +0300

    update user dto

--- E_commerceManagementSystem.BLL/Dto/AccountDto/UserRegisterDto.cs
+++ E_commerceManagementSystem.BLL/Dto/AccountDto/UserRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace E_commerceManagementSystem.BLL.DTOs.AccountDto
 {
     public class UserRegisterDTO
     {
+        [MinLength(3,ErrorMessage = "The field UserName must be a string with a minimum length of 3") ]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "Email address can't be embty")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Password must be at least 4 characters long.")]
         public string Password { get; set; }
+        [Required]
+        [Compare("Password",ErrorMessage = "Password don't match with ConfirmPassword")]
         public string ConfirmPassWord { get; set; }
+        [Required]
         public string Phone { get; set; }
     }
 }
