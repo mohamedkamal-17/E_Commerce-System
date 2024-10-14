374540738c38fa230f223553691a07dbafcd604a
mmohamedkamal-17 <mmohamedkamal17@gmail.com>
2024-10-14 17:11:16 +0300

    Update Data base

---
+++ E_commerceManagementSystem.DAL/Migrations/20241014134933_addIsDeletedPropperty.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_commerceManagementSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIsDeletedPropperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Shipping",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Reviews",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Payments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OrderItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Inventory",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Carts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "CartItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Shipping");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Inventory");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetUsers");
+        }
+    }
+}
--- E_commerceManagementSystem.DAL/Reposatories/GeneralRepository/Repository.cs
+++ E_commerceManagementSystem.DAL/Reposatories/GeneralRepository/Repository.cs
@@ -3,6 +3,7 @@ using E_commerceManagementSystem.DAL.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -37,9 +38,43 @@ namespace E_commerceManagementSystem.DAL.Reposatories.GeneralRepository
             return query;
         }
 
+
         public async Task<T> GetByIdAsync(int id)
         {
             return await _dbSet.FindAsync(id);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        // 2. Virtual method to get all records with a condition
+        public virtual IQueryable<T> GetAll(Expression<Func<T, bool>> condition)
+        {
+            return _dbSet.Where(condition);
+        }
+
+        // 3. Virtual method to get all records with includes
+        public virtual IQueryable<T> GetAll(params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);  // Assuming Include is available for IQueryable
+            }
+            return query;
+        }
+
+        // 4. Virtual method to get all records with a condition and includes
+        public virtual IQueryable<T> GetAll(Expression<Func<T, bool>> condition, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet.Where(condition);
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return query;
         }
 
         public async Task AddAsync(T entity)
